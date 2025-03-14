137216eb2d519615881d23282cfa4e7123a94f26
Jhonnathandc <corajhonnathan5@gmail.com>
2025-03-14 19:56:12 -0300

    teste

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,7 @@
-export default () => ({});
+module.exports = {
+  'users-permissions': {
+    config: {
+      jwtSecret: 'I9jtJfmOrqQTo4ucZrw6Gg==', // Substitua pelo valor gerado
+    },
+  },
+};
