412043425da9fc911dc2d56d79dfb197a8c7c62b
Jhonnathandc <corajhonnathan5@gmail.com>
2025-03-15 00:07:47 -0300

    add formatod cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -14,7 +14,12 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['*'],
+      origin: [
+        'https://www.formatod.com.br',
+        'http://127.0.0.1:5500', // Frontend local
+        'http://localhost:1337', // Painel admin local
+        'https://cms-strapi-backend-puxh.onrender.com', // Backend no Render
+      ],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
       headers: ['Content-Type', 'Authorization'],
       keepHeadersOnError: true,
